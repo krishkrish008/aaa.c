f4522d6d3a11bd8a60557fdeb036396d26e99e67
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-01-22 16:41:02 +0530

    kkkkkkk.c

---
+++ gyugygjyi.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,power;
+printf("Enter a");
+scanf("%d",&a);
+printf("Enter b");
+scanf("%d",&b);
+power=a^b;
+printf("Power value is %d",power);
+return 0;
+}
